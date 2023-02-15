a8044ce19fe2ed5c5716c3e61365cbb13bd75d95
Chris Ritter <chriswritter@gmail.com>
2023-02-14 20:15:08 -0500

    Add project files.

---
+++ Software2KnowledgeCheck1/City.cs
@@ -0,0 +1,11 @@
+﻿namespace Software2KnowledgeCheck1
+    {
+    internal class City
+        {
+        public int ZipCode { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public int Population { get; set; }
+
+        }
+    }
